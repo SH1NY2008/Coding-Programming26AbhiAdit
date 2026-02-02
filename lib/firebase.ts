519ba266e0417084521f693dc52b97b62e834161
SH1NY2008 <79332062+SH1NY2008@users.noreply.github.com>
2026-02-02 02:48:27 -0600

    dang

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAufYb_3y45jljQLIgeauVIAKtYEtGZqsw",
-  authDomain: "coding-programming26.firebaseapp.com",
-  projectId: "coding-programming26",
-  storageBucket: "coding-programming26.firebasestorage.app",
-  messagingSenderId: "104352134515",
-  appId: "1:104352134515:web:9b7bf25aedf019eae00106",
-  measurementId: "G-H30Y1J46KN"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
 
 // Initialize Firebase
